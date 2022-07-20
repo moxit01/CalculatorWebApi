fb9500ce14359ba67c9e72f64834b36ad4191e8e
Moxit <shah.moxit999@gmail.com>
2022-07-20 18:27:02 -0400

    Calculator logic with addition task

---
+++ CalculatorWebApi/CalculatorLogic/AddLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CalculatorLogic
+{
+	public class AddLogic
+	{
+		public static double Addition(double num1, double num2)
+		{
+			return num1 + num2;
+		}
+	}
+}
+
---
+++ CalculatorWebApi/CalculatorWebApi/Controllers/CalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CalculatorLogic;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CalculatorWebApi.Controllers
+{
+
+   
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        // GET: /<controller>/
+        [HttpGet]
+        public double Add([FromQuery] double num1, [FromQuery] double num2)
+            {
+                return AddLogic.Addition(num1, num2);
+            }
+     
+    }
+}
+
