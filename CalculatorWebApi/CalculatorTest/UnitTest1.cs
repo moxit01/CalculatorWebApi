aba78a60569d69c168bbfbc20dc5133846a53790
Moxit <shah.moxit999@gmail.com>
2022-07-20 19:31:25 -0400

    Added unitest for subtraction

--- CalculatorWebApi/CalculatorTest/UnitTest1.cs
+++ CalculatorWebApi/CalculatorTest/UnitTest1.cs
@@ -123,4 +123,142 @@ public class UnitTest1
 
         Assert.AreEqual(desired, actualResult);
     }
+
+
+    /// <summary>
+    /// Subtraction Test
+    /// </summary>
+
+    [TestMethod]
+    public void SubtractionUnitTestCheck1()
+    {
+        double num1 = 7;
+        double num2 = 4;
+        double desired = 3;
+
+        double actualResult = Logic.Subtraction(num1, num2);
+
+        Assert.AreEqual(desired, actualResult);
+    }
+
+    //Test 2
+    [TestMethod]
+    public void SubtractionUnitTestCheck2()
+    {
+        double num1 = 11;
+        double num2 = 3;
+        double desired = 8;
+
+        double actualResult = Logic.Subtraction(num1, num2);
+
+        Assert.AreEqual(desired, actualResult);
+    }
+
+    //Test 3
+    [TestMethod]
+    public void SubtractionUnitTestCheck3()
+    {
+        double num1 = 15;
+        double num2 = 4;
+        double desired = 11;
+
+        double actualResult = Logic.Subtraction(num1, num2);
+
+        Assert.AreEqual(desired, actualResult);
+    }
+
+    //Test 4
+    [TestMethod]
+    public void SubtractionUnitTestCheck4()
+    {
+        double num1 = 23;
+        double num2 = 11;
+        double desired = 12;
+
+        double actualResult = Logic.Subtraction(num1, num2);
+
+        Assert.AreEqual(desired, actualResult);
+    }
+
+    //Test 5
+    [TestMethod]
+    public void SubtractionUnitTestCheck5()
+    {
+        double num1 = 33;
+        double num2 = 9;
+        double desired = 24;
+
+        double actualResult = Logic.Subtraction(num1, num2);
+
+        Assert.AreEqual(desired, actualResult);
+    }
+
+    //Test 6
+    [TestMethod]
+    public void SubtractionUnitTestCheck6()
+    {
+        double num1 = 17;
+        double num2 = 11;
+        double desired = 6;
+
+        double actualResult = Logic.Subtraction(num1, num2);
+
+        Assert.AreEqual(desired, actualResult);
+    }
+
+    //Test 7
+    [TestMethod]
+    public void SubtractionUnitTestCheck7()
+    {
+        double num1 = 53;
+        double num2 = 7;
+        double desired = 46;
+
+        double actualResult = Logic.Subtraction(num1, num2);
+
+        Assert.AreEqual(desired, actualResult);
+    }
+
+    //Test 8
+    [TestMethod]
+    public void SubtractionUnitTestCheck8()
+    {
+        double num1 = 61;
+        double num2 = 3;
+        double desired = 58;
+
+        double actualResult = Logic.Subtraction(num1, num2);
+
+        Assert.AreEqual(desired, actualResult);
+    }
+
+    //Test 9
+    [TestMethod]
+    public void SubtractionUnitTestCheck9()
+    {
+        double num1 = 77;
+        double num2 = 5;
+        double desired = 72;
+
+        double actualResult = Logic.Subtraction(num1, num2);
+
+        Assert.AreEqual(desired, actualResult);
+    }
+
+    //Test 10
+    [TestMethod]
+    public void SubtractionUnitTestCheck10()
+    {
+        double num1 = 88;
+        double num2 = 11;
+        double desired = 77;
+
+        double actualResult = Logic.Subtraction(num1, num2);
+
+        Assert.AreEqual(desired, actualResult);
+    }
+
+
+
+
 }
